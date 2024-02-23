a09addf292a089b85a8fdba70357f75a67813eaa
Abdul Ahad <129857978+abdulahad139@users.noreply.github.com>
2024-02-23 16:07:43 +0500

    updated Feb 23, 2024

---
+++ main.ts
@@ -0,0 +1,14 @@
+// Question 2
+// let person = "Abdul Ahad";
+// console.log('Hello',person ,'would you like to learn some Python today?');
+
+//Question 3
+// let person = "Abdul Ahad";
+// console.log("Upper Case",person.toUpperCase());
+// console.log("Lower Case",person.toLowerCase());
+// console.log("Title Case",person.replace(/\b\w/g, c => c.toUpperCase()));
+
+//Question 4
+// let famous_person = "Socrates";
+// let quote = "'It's shame for a man to grow old without seeing the beauty and strength of which his body is capable.'";
+// console.log(famous_person,"once said,",quote);
