1733c0d07e39101b6e052f79ec8212706a8a589a
Abdul Ahad <129857978+abdulahad139@users.noreply.github.com>
2024-05-29 21:51:36 +0500

    uploading tasks

---
+++ task27.js
@@ -0,0 +1,34 @@
+var alien;
+// This version will true the green condition
+alien = "green";
+if (alien == "green") {
+    console.log("You've just earned 5 points.");
+}
+else if (alien == "yellow") {
+    console.log("You've earned 10 points");
+}
+else if (alien == "red") {
+    console.log("You've just earned 15");
+}
+// This version will true the yellow condition
+alien = "yellow";
+if (alien == "green") {
+    console.log("You've just earned 5 points.");
+}
+else if (alien == "yellow") {
+    console.log("You've earned 10 points");
+}
+else if (alien == "red") {
+    console.log("You've just earned 15");
+}
+// This version will true the red condition
+alien = "red";
+if (alien == "green") {
+    console.log("You've just earned 5 points.");
+}
+else if (alien == "yellow") {
+    console.log("You've earned 10 points");
+}
+else if (alien == "red") {
+    console.log("You've just earned 15");
+}
